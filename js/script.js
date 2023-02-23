0c25f1f8e08a6ccdc692e0649df223e4cd2974db
ziggibot <till_kappus@gmx.de>
2023-02-23 13:56:40 +0100

    added choice for row number

--- js/script.js
+++ js/script.js
@@ -4,34 +4,25 @@ const gridContainer = document.querySelector('#grid_container');
 const gridSquares = gridContainer.childNodes;
 const squaresButton = document.querySelector('#squares_button');
 
+
 squaresButton.addEventListener('click', function() {
-    prompt("How many squares per row?");
+    const squaresNumber = parseInt(prompt("How many squares per row?"));
+
+    for (i = 0; i < Math.pow(squaresNumber, 2); i++) {
+        let divGrid = document.createElement('div');
+
+        squareSize = parseInt(gridContainer.offsetWidth / squaresNumber);
+
+        divGrid.style.height = squareSize + 'px';
+        divGrid.style.width = squareSize + 'px';
+        divGrid.style.border = "0.01rem solid black";
+        divGrid.style.boxSizing = "border-box";
+    
+        gridContainer.appendChild(divGrid);
+    }
+    gridSquares.forEach(function(i) {
+        i.addEventListener('mouseover', function() {
+            this.style.backgroundColor = "red";
+        })
 });
-
-
-
-
-
-
-
-
-
-
-
-for (i = 0; i < 256; i++) {
-    let divGrid = document.createElement('div');
-
-    divGrid.style.height = "1rem";
-    divGrid.style.width = "1rem";
-    divGrid.style.border = "0.01rem solid black";
-    divGrid.style.boxSizing = "border-box";
-
-    gridContainer.appendChild(divGrid);
-
-}
-
-gridSquares.forEach(function(i) {
-    i.addEventListener('mouseover', function() {
-        this.style.backgroundColor = "red";
-    })
 })
