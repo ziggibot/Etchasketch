05b0e7253ca7051a6a0c66d54efa0ac465d52892
ziggibot <till_kappus@gmx.de>
2023-02-23 10:13:19 +0100

    added mouseover effect to grid squares

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
+
+
+
+
+
 const gridContainer = document.querySelector('#grid_container');
+const gridSquares = gridContainer.childNodes;
+
+
 
 for (i = 0; i < 256; i++) {
     let divGrid = document.createElement('div');
 
-    divGrid.style.backgroundColor = "red";
-    divGrid.style.height = "2rem";
-    divGrid.style.width = "2rem";
-    divGrid.style.border = "0.1rem solid black";
+    divGrid.style.height = "1rem";
+    divGrid.style.width = "1rem";
+    divGrid.style.border = "0.01rem solid black";
     divGrid.style.boxSizing = "border-box";
-    
 
     gridContainer.appendChild(divGrid);
+
 }
 
 
+gridSquares.forEach(function(i) {
+    i.addEventListener('mouseover', function() {
+        this.style.backgroundColor = "red";
+    })
+})
