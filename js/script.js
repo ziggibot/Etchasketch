c8dc4ff4f8331ee4a18806e925929887fecbcbee
ziggibot <till_kappus@gmx.de>
2023-02-23 09:22:38 +0100

    set up grid

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 const gridContainer = document.querySelector('#grid_container');
 
-for (i = 0; i <= 256; i++) {
+for (i = 0; i < 256; i++) {
     let divGrid = document.createElement('div');
 
-    divGrid.style.width = "5rem";
-    divGrid.style.height = "5rem";
     divGrid.style.backgroundColor = "red";
-    divGrid.style.border = "solid black 0.5em";
+    divGrid.style.height = "2rem";
+    divGrid.style.width = "2rem";
+    divGrid.style.border = "0.1rem solid black";
+    divGrid.style.boxSizing = "border-box";
+    
 
     gridContainer.appendChild(divGrid);
 }
